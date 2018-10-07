e9f5daa837e7ff275df11ae808e385f2797729e5
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-10-07 18:53:29 -0500

    get songs by artist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { SongsComponent } from './components/songs/songs.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistComponent,
-    SongsComponent,
-    ListComponent
+    SongsComponent
   ],
   imports: [
     BrowserModule,
